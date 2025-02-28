7b118ad4bb62baacb9598b8e6c35d45e42401123
Cat-In-Case <52603485+Cat-In-Case@users.noreply.github.com>
2025-02-28 16:09:54 +0900

    c

--- Dictionary/SerializableConcurrentDictioanry.cs
+++ Dictionary/SerializableConcurrentDictioanry.cs
@@ -1,15 +1,197 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using ArchiementTest;
 using UnityEngine;
+using System;
+using static UnityEditor.Progress;
+using static UnityEngine.Rendering.DebugUI;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
-namespace Assets.Resourc.___자료구조
+//Immutable할 이유가 없음
+public class SerializableConcurrentDictioanry<Key, Value> : ISerializationCallbackReceiver, IDictionary<Key, Value>, IReadOnlyDictionary<Key, Value>
 {
-    public class SerializableConcurrentDictioanry<K, V> : MonoBehaviour
+    //Serializable
+#if UNITY_EDITOR
+    [System.Serializable]
+    private struct KVP<Key, Value>
     {
-
-        [System.Serializable]
-        private struct KVP<K, V>
+        internal KVP(KeyValuePair<Key, Value> pair)
         {
-            public K key; public V value;
+            key = pair.Key;
+            value = pair.Value;
+        }
+        internal Key key; internal Value value;
+    }
+    [Header("Immutable Data(Can not change)")]
+    [SerializeField] private List<KVP<Key, Value>> _serializedData;
+    public void OnBeforeSerialize()     // Dic => List
+    {
+        if (_serializedData == null || _internal == null)
+            return;
+        _serializedData.Clear();
+        IEnumerable<KeyValuePair<Key, Value>> ienumerable = _internal;
+        foreach (var kvp in ienumerable)
+        {
+            _serializedData.Add(new KVP<Key, Value>(kvp));
         }
     }
+    public void OnAfterDeserialize()    //List => Dic
+    {
+        if (_serializedData == null || _internal == null)
+            return;
+        _internal.Clear();
+        for (int i = 0; i < _serializedData.Count; i++)
+        {
+            if (_internal.TryAdd(_serializedData[i].key, _serializedData[i].value) == false)
+            {
+                throw new System.ArgumentException("KeyValuePair Error");
+            }
+        }
+    }
+
+#else
+    public void OnAfterDeserialize() { }
+    public void OnBeforeSerialize() { }
+#endif
+
+    private SerializableConcurrentDictioanry() { }
+
+    private SerializableConcurrentDictioanry(ConcurrentDictionary<Key, Value> dictionary)
+    {
+        this._internal = dictionary;
+    }
+
+    public static SerializableConcurrentDictioanry<Key, Value> Create(IDictionary<Key, Value> dic)
+    {
+        if (dic == null)
+            throw new NullReferenceException("Dictionary is Null");
+        if(dic.Count == 0)
+            throw new Exception("Dictionary is Empty");
+        ConcurrentDictionary<Key, Value> cd = new ConcurrentDictionary<Key, Value>(dic);
+        return new SerializableConcurrentDictioanry<Key, Value>(cd);
+    }
+    public static SerializableConcurrentDictioanry<Key, Value> Create(IEnumerable<KeyValuePair<Key, Value>> pairs)
+    {
+        if (pairs == null)
+            throw new NullReferenceException("Dictionary is Null");
+        if (pairs.Count() == 0)
+            throw new Exception("Dictionary is Empty");
+        ConcurrentDictionary<Key, Value> cd = new ConcurrentDictionary<Key, Value>(pairs);
+        return new SerializableConcurrentDictioanry<Key, Value>(cd);
+    }
+
+    private readonly ConcurrentDictionary<Key, Value> _internal;        //느려터짐..
+
+    //Type Restricted Funcs
+    public Value this[Key key] { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public bool TryGetValue(Key key, out Value value)
+    {
+        return _internal.TryGetValue(key, out value);
+    }
+
+    public int Count => _internal.Count;
+
+    public bool IsReadOnly => false;
+
+    public IDictionary<Key, Value> Duplicate { get { return new Dictionary<Key, Value>(_internal); } }
+
+    public void Add(Key key, Value value)
+    {
+        try
+        {
+            _internal.TryAdd(key, value);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+    public void Add(KeyValuePair<Key, Value> item)
+    {
+        try
+        {
+            _internal.TryAdd(item.Key, item.Value);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+    public bool TryAdd(Key key, Value value)
+    {
+        return _internal.TryAdd(key, value);
+    }
+    public bool TryAdd(KeyValuePair<Key, Value> item)
+    {
+        return _internal.TryAdd(item.Key, item.Value);
+    }
+
+    public Value AddOrUpdate(Key key, Value value, Func<Key, Value, Value> updateValueFactory)
+    {
+        return _internal.AddOrUpdate(key, value, updateValueFactory);
+    }
+    public Value AddOrUpdate(Key key, Func<Key, Value> addValueFactory, Func<Key, Value, Value> updateValueFactory)
+    {
+        return _internal.AddOrUpdate(key, addValueFactory, updateValueFactory);
+    }
+    public bool Contains(KeyValuePair<Key, Value> item)
+    {
+        return _internal.ContainsKey(item.Key);
+    }
+    public bool ContainsKey(Key key)
+    {
+        return _internal.ContainsKey(key);
+    }
+    public void Clear()
+    {
+        _internal.Clear();
+    }
+
+
+
+    public bool Remove(Key key)
+    {
+        return _internal.Remove(key, out _);
+    }
+
+    public bool Remove(KeyValuePair<Key, Value> item)
+    {
+        return _internal.Remove(item.Key, out _);
+    }
+
+    public bool TryRemove(Key key, out Value value)
+    {
+        return _internal.Remove(key, out value);
+    }
+
+    public bool TryRemove(KeyValuePair<Key, Value> item, out Value value)
+    {
+        return _internal.Remove(item.Key, out value);
+    }
+
+
+
+    public void CopyTo(KeyValuePair<Key, Value>[] array, int arrayIndex)
+    {
+        
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _internal.GetEnumerator();
+    }
+
+    public IEnumerator<KeyValuePair<Key, Value>> GetEnumerator()
+    {
+        return _internal.GetEnumerator();
+    }
+    public ICollection<Key> Keys => _internal.Keys;
+
+    public ICollection<Value> Values => _internal.Values;
+    IEnumerable<Key> IReadOnlyDictionary<Key, Value>.Keys => _internal.Keys;
+
+    IEnumerable<Value> IReadOnlyDictionary<Key, Value>.Values => _internal.Values;
 }
